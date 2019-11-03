bbfbf981d0023486ec31055a9cb8ea155643d3f0
JoseRobertoECJr <joserobertoecjr@gmail.com>
2019-11-03 20:13:54 -0300

    +estrutura recursiva do round

--- player-ia_player.py
+++ player-ia_player.py
@@ -2,46 +2,61 @@ from models.move import Move
 from anytree import Node, RenderTree, NodeMixin
 
 MAX_ROUNDS = 2
+ROUNDS = 1
 EMPTY, BLACK, WHITE, OUTER = '.', '@', 'o', '?'
 
 
 class Round(NodeMixin):
-  def __init__(self, player, parent, isMyTurn):
-    self.board = Board(None)
-    self.player = player
+  def __init__(self, color, board, parent=None):
+    self.board = board
+    self.color = color
 
-  def getMovement():
+  def getMovementValue(self, isMyTurn):
 
-    validMovesLength = len(self.board.valid_moves(self.color))
-    if validMovesLength <= 0:
-      return self.getMinMaxValue()
+    validMoves = self.board.valid_moves(self.color)
 
-  def getMinMaxValue():
+    if len(validMoves) <= 0 or ROUNDS >= MAX_ROUNDS :
+      return isMyTurn * self.getMinMaxValue()
+    else:
+      movValue = None
+      
+      for move in  validMoves:
+        movTempValue = Round(self.color, self.board.get_clone().play(move), self).getMovementValue(isMyTurn)
+        if movValue == None or (isMyTurn * movValue < isMyTurn * movTempValue):
+          movValue = movTempValue
+
+      return movValue
+
+  def getMinMaxValue(self):
     score = self.board.score()
-    if self.player.color == WHITE:
+    if self.color == WHITE:
       return score[0] - score[1]
     else:
       return score[1] - score[0]
+
+
 
 class HumanPlayer:
   
   def __init__(self, color):
     self.color = color
 
-
   def play(self, board):
   
-    rowInp = int(raw_input("Linha: "))
-    colInp = int(raw_input("Coluna: "))
-    move = Move(rowInp, colInp)
-
-    prof = 0
-    while prof < self.prof
-      board.valid_moves(self.color)
-      self.board.play(self.atual_player.play(self.board.get_clone()), atual_color)
-      prof++
-
-    while move not in board.valid_moves(self.color):
-      move = Move(rowInp, colInp)
+    move = self.getMovement(board)
 	  
     return move
+
+  def getMovement(self, board):
+
+    validMoves = board.valid_moves(self.color)
+    movValue = None
+    myMove = None
+
+    for move in  validMoves:
+      movTempValue = Round(self.color, board.get_clone().play(move)).getMovementValue(1)
+      if movValue == None or movValue < movTempValue:
+        myMove = move
+        movValue = movTempValue
+
+    return myMove
