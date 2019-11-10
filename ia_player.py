abc4d967ae24e7db11bba966e76a8b96a378a81d
JoseRobertoECJr <joserobertoecjr@gmail.com>
2019-11-09 21:49:39 -0300

    #second player

--- ia_player.py
+++ ia_player.py
@@ -4,44 +4,60 @@ class IaPlayer:
   
   import time
 
-  def getMovementValue(self, color, board, isMyTurn, roundNum, maxRounds):
+  def getMovementValue(self, color, isMyTurn, board, roundNum, fatherValue):
 
     validMoves = board.valid_moves(color)
 
     now = self.time.time()
     time = now - self.start
     
-    if len(validMoves) <= 0 or roundNum >= maxRounds or time > 2.9 :
-      return isMyTurn * self.getMinMaxValue(board)
+    if len(validMoves) <= 0 or roundNum >= self.maxRounds or time > 2.9 :
+      return self.getMinMaxValue(board)
     else:
       movValue = None
+      colorTemp = '@'
+      if color == '@':
+        colorTemp = 'o'
+
       for move in  validMoves:
         boardClone = board.get_clone()
         boardClone.play(move, color)
 
-        colorTemp = 'o'
-        if color == colorTemp:
-          colorTemp = '@'
-
-        movTempValue = self.getMovementValue(colorTemp, boardClone,-isMyTurn, roundNum + 1, maxRounds)
+        movTempValue = self.getMovementValue(colorTemp, isMyTurn, boardClone, roundNum + 1, movValue)
 
         if movValue == None or (isMyTurn * movValue < isMyTurn * movTempValue):
           movValue = movTempValue
+          if fatherValue != None and (isMyTurn * fatherValue < isMyTurn * movValue):
+            return movTempValue;
 
       return movValue
 
   def getMinMaxValue(self, board):
-    score = board.score()
+    score = self.score(board)
     if self.color == 'o':
       return score[0] - score[1]
     else:
       return score[1] - score[0]
 
+  def score(self, board):
+    white = 0
+    black = 0
+    for i in range(1, 9):
+      for j in range(1, 9):
+        fator = 1
+        if i == 1 or  i == 8 or j == 1 or j == 8:
+          fator = 3
+        if board.board[i][j] == 'o':
+          white += (1 * fator)
+        elif board.board[i][j] == '@':
+          black += (1 * fator)
 
+    return [white, black]
 
 
   def __init__(self, color):
     self.color = color
+    self.maxRounds = 10
 
   def play(self, board):
   
@@ -51,10 +67,14 @@ class IaPlayer:
 
     self.start = self.time.time()
 
+    colorTemp = '@'
+    if self.color == '@':
+      colorTemp = 'o'
+
     for move in  validMoves:
       boardClone = board.get_clone()
       boardClone.play(move, self.color)
-      movTempValue = self.getMovementValue(self.color, boardClone, 1, 1, 10)
+      movTempValue = self.getMovementValue(colorTemp, -1, boardClone, 1, movValue)
       if movValue == None or movValue < movTempValue:
         myMove = move
         movValue = movTempValue
